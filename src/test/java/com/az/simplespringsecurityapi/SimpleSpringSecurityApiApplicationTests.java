5374e8632c525422556847f149d484f536a59bf6
Az Mayo <AzharMobeen@gmail.com>
2019-12-11 08:59:49 +0400

    Initial Commit

---
+++ src/test/java/com/az/simplespringsecurityapi/SimpleSpringSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az.simplespringsecurityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringSecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
